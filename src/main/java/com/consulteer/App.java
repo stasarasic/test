26332d48a2338a37b527107dff42cc5a36397184
Stasa <stasa.rasic@consulteer.com>
2016-05-30 14:03:23 +0200

    Init commit

---
+++ src/main/java/com/consulteer/App.java
@@ -0,0 +1,12 @@
+package com.consulteer;
+
+/**
+ * Created by stasa on 30.5.16..
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
